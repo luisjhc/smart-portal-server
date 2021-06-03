393e6d4422dc079af58a708bc2db404fe9ff0b06
luisjhc <ljoaquincuenca@gmail.com>
2021-06-03 19:26:59 +0200

    solving issues

--- routes/myPortal.js
+++ routes/myPortal.js
@@ -3,79 +3,9 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 const Content = require("../models/Content.model");
 const router = require("express").Router();
 
-// ℹ️ Handles password encryption
-const bcrypt = require("bcryptjs");
-const mongoose = require("mongoose");
-
-// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
-const saltRounds = 10;
-
-// Require the User model in order to interact with the database
-const User = require("../models/User.model");
-const Session = require("../models/Session.model");
-
-const isLoggedIn = require("../middleware/isLoggedIn");
-
-router.get("/myPortal", isLoggedIn, (req, res) => {
+router.get("/content", (req, res) => {
   Content.find({}).then((allContent) => {
     res.json(allContent);
-  });
-});
-
-router.post("/createStudent", isLoggedIn, (req, res) => {
-  const { username, password } = req.body;
-  console.log("req.body:", req.body);
-
-  if (!username) {
-    return res
-      .status(400)
-      .json({ errorMessage: "Please provide your username." });
-  }
-
-  if (password.length < 8) {
-    return res.status(400).json({
-      errorMessage: "Your password needs to be at least 8 characters long.",
-    });
-  }
-
-  // Search the database for a user with the username submitted in the form
-  User.findOne({ username }).then((found) => {
-    // If the user is found, send the message username is taken
-    if (found) {
-      return res.status(400).json({ errorMessage: "Username already taken." });
-    }
-
-    // if user is not found, create a new user - start with hashing the password
-    return bcrypt
-      .genSalt(saltRounds)
-      .then((salt) => bcrypt.hash(password, salt))
-      .then((hashedPassword) => {
-        // Create a user and save it in the database
-        return User.create({
-          username,
-          password: hashedPassword,
-        });
-      })
-      .then((user) => {
-        Session.create({
-          user: user._id,
-          createdAt: Date.now(),
-        }).then((session) => {
-          res.status(201).json({ user, accessToken: session._id });
-        });
-      })
-      .catch((error) => {
-        if (error instanceof mongoose.Error.ValidationError) {
-          return res.status(400).json({ errorMessage: error.message });
-        }
-        if (error.code === 11000) {
-          return res.status(400).json({
-            errorMessage:
-              "Username need to be unique. The username you chose is already in use.",
-          });
-        }
-        return res.status(500).json({ errorMessage: error.message });
-      });
   });
 });
 
